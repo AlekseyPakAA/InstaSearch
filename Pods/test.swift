ba380a32ef254bcdd98e6cb765a9fe26623624cc
admin <admin@iMac.local>
2017-07-06 05:32:44 +0300

    123

---
+++ Pods/test.swift
@@ -0,0 +1,19 @@
+//
+//  test.swift
+//  Pods
+//
+//  Created by admin on 04.07.17.
+//
+//
+
+import Foundation
+
+
+class test {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
